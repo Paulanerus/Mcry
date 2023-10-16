3eae877bd55f3cfb46bef5018e6330ef50a494c4
Paul <41129807+Paulanerus@users.noreply.github.com>
2023-10-16 20:56:24 +0200

    Small changes in medusa

--- include/medusa/medusa.hpp
+++ include/medusa/medusa.hpp
@@ -1,6 +1,14 @@
 #pragma once
 
-#include <cstddef>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <vector>
+
+#include <iostream>
 
 namespace Mcry
 {
@@ -10,10 +18,33 @@ namespace Mcry
         {
         public:
             Medusa(size_t pool_size);
+
             ~Medusa();
 
+            template <typename T, typename... Args>
+            auto submit(const T &item, Args &&...args)
+            {
+                std::packaged_task<decltype(item(args...))()> task{std::bind(item, std::forward<Args>(args)...)};
+
+                {
+                    std::unique_lock<std::mutex> lock{m_Mutex};
+                }
+
+                return task.get_future();
+            }
+
         private:
-            size_t m_PoolSize;
+            const size_t m_PoolSize;
+
+            bool m_IsRunning{true};
+
+            std::condition_variable m_ConditionVariable;
+
+            std::mutex m_Mutex;
+
+            std::vector<std::thread> m_Threads;
+
+            std::queue<std::function<void()>> m_Jobs;
         };
     }
 }
--- src/medusa/medusa.cpp
+++ src/medusa/medusa.cpp
@@ -8,10 +8,30 @@ namespace Mcry
     {
         Medusa::Medusa(size_t pool_size) : m_PoolSize{pool_size}
         {
+            auto worker = [this]
+            {
+                while (!m_IsRunning)
+                {
+                    std::unique_lock<std::mutex> lock{m_Mutex};
+                    m_ConditionVariable.wait(lock, [this]
+                                             { return !m_Jobs.empty() || !m_IsRunning; });
+
+                }
+            };
+
+            for (size_t i{}; i < pool_size; i++)
+                m_Threads.emplace_back(worker);
         }
 
         Medusa::~Medusa()
         {
+            m_IsRunning = false;
+
+            m_ConditionVariable.notify_all();
+
+            for (auto &thread : m_Threads)
+                thread.join();
         }
+
     }
 }
