09f6b3f202ca93ee0ecdf623d56427fccdb4bec9
Paul <41129807+Paulanerus@users.noreply.github.com>
2023-11-17 11:51:34 +0100

    Add new methods and update MBuffer class

--- include/buffer/mbuffer.hpp
+++ include/buffer/mbuffer.hpp
@@ -1,16 +1,123 @@
 #pragma once
 
+#include <cstdint>
+#include <string_view>
 #include <cstddef>
+#include <vector>
 
 namespace Mcry
 {
-
     class MBuffer
     {
     public:
-        const size_t size() noexcept;
+        MBuffer() noexcept = default;
+
+        MBuffer(size_t size);
+
+        size_t size() const noexcept;
+
+        void clear() noexcept;
+
+        bool pop_front(size_t size = 1);
+
+        bool pop_back(size_t size = 1) noexcept;
+
+        MBuffer &put(uint8_t val) noexcept;
+
+        MBuffer &put(int8_t val) noexcept;
+
+        MBuffer &put(uint16_t val) noexcept;
+
+        MBuffer &put(int16_t val) noexcept;
+
+        MBuffer &put(uint32_t val) noexcept;
+
+        MBuffer &put(int32_t val) noexcept;
+
+        MBuffer &put(uint64_t val) noexcept;
+
+        MBuffer &put(int64_t val) noexcept;
+
+        MBuffer &put(float val) noexcept;
+
+        MBuffer &put(double val) noexcept;
+
+        MBuffer &put(bool val) noexcept;
+
+        bool get(uint8_t &val, size_t index = 0) const noexcept;
+
+        bool get(int8_t &val, size_t index = 0) const noexcept;
+
+        bool get(uint16_t &val, size_t index = 0) const noexcept;
+
+        bool get(int16_t &val, size_t index = 0) const noexcept;
+
+        bool get(uint32_t &val, size_t index = 0) const noexcept;
+
+        bool get(int32_t &val, size_t index = 0) const noexcept;
+
+        bool get(uint64_t &val, size_t index = 0) const noexcept;
+
+        bool get(int64_t &val, size_t index = 0) const noexcept;
+
+        bool get(float &val, size_t index = 0) const noexcept;
+
+        bool get(double &val, size_t index = 0) const noexcept;
+
+        bool get(bool &val, size_t index = 0) const noexcept;
+
+        uint8_t operator[](size_t index) const noexcept;
+
+        MBuffer &operator<<(uint8_t val) noexcept;
+
+        MBuffer &operator<<(int8_t val) noexcept;
+
+        MBuffer &operator<<(uint16_t val) noexcept;
+
+        MBuffer &operator<<(int16_t val) noexcept;
+
+        MBuffer &operator<<(uint32_t val) noexcept;
+
+        MBuffer &operator<<(int32_t val) noexcept;
+
+        MBuffer &operator<<(uint64_t val) noexcept;
+
+        MBuffer &operator<<(int64_t val) noexcept;
+
+        MBuffer &operator<<(float val) noexcept;
+
+        MBuffer &operator<<(double val) noexcept;
+
+        MBuffer &operator<<(bool val) noexcept;
+
+        void operator>>(uint8_t &val) const noexcept;
+
+        void operator>>(int8_t &val) const noexcept;
+
+        void operator>>(uint16_t &val) const noexcept;
+
+        void operator>>(int16_t &val) const noexcept;
+
+        void operator>>(uint32_t &val) const noexcept;
+
+        void operator>>(int32_t &val) const noexcept;
+
+        void operator>>(uint64_t &val) const noexcept;
+
+        void operator>>(int64_t &val) const noexcept;
+
+        void operator>>(float &val) const noexcept;
+
+        void operator>>(double &val) const noexcept;
+
+        void operator>>(bool &val) const noexcept;
 
     private:
-        size_t m_Size;
+        MBuffer &put(uint8_t *data, size_t size) noexcept;
+
+        bool hasEnoughSpace(size_t index, size_t size) const noexcept;
+
+    private:
+        mutable std::vector<uint8_t> m_Buffer;
     };
 }
--- src/buffer/mbuffer.cpp
+++ src/buffer/mbuffer.cpp
@@ -1,9 +1,365 @@
 #include "buffer/mbuffer.hpp"
 
+#include "net/mnet.hpp"
+
 namespace Mcry
 {
-    const size_t MBuffer::size() noexcept
+    MBuffer::MBuffer(size_t size)
     {
-        return m_Size;
+        m_Buffer.reserve(size);
+    }
+
+    size_t MBuffer::size() const noexcept
+    {
+        return m_Buffer.size();
+    }
+
+    void MBuffer::clear() noexcept
+    {
+        m_Buffer.clear();
+    }
+
+    bool MBuffer::pop_front(size_t size)
+    {
+        if (!hasEnoughSpace(0, size))
+            return false;
+
+        m_Buffer.erase(m_Buffer.begin(), m_Buffer.begin() + size);
+
+        return true;
+    }
+
+    bool MBuffer::pop_back(size_t size) noexcept
+    {
+        if (!hasEnoughSpace(0, size))
+            return false;
+
+        for (size_t i{}; i < size; i++)
+            m_Buffer.pop_back();
+
+        return true;
+    }
+
+    MBuffer &MBuffer::put(uint8_t val) noexcept
+    {
+        return put(&val, sizeof(uint8_t));
+    }
+
+    MBuffer &MBuffer::put(int8_t val) noexcept
+    {
+        return put(reinterpret_cast<uint8_t *>(&val), sizeof(int8_t));
+    }
+
+    MBuffer &MBuffer::put(uint16_t val) noexcept
+    {
+        auto val_no = htons(val);
+
+        return put(reinterpret_cast<uint8_t *>(&val_no), sizeof(uint16_t));
+    }
+
+    MBuffer &MBuffer::put(int16_t val) noexcept
+    {
+        auto val_no = static_cast<int16_t>(htons(val));
+
+        return put(reinterpret_cast<uint8_t *>(&val_no), sizeof(int16_t));
+    }
+
+    MBuffer &MBuffer::put(uint32_t val) noexcept
+    {
+        auto val_no = htonl(val);
+
+        return put(reinterpret_cast<uint8_t *>(&val_no), sizeof(uint32_t));
+    }
+
+    MBuffer &MBuffer::put(int32_t val) noexcept
+    {
+        auto val_no = static_cast<int32_t>(htonl(val));
+
+        return put(reinterpret_cast<uint8_t *>(&val_no), sizeof(int32_t));
+    }
+
+    MBuffer &MBuffer::put(uint64_t val) noexcept
+    {
+        auto val_no = Net::HtoNll(val);
+
+        return put(reinterpret_cast<uint8_t *>(&val_no), sizeof(uint64_t));
+    }
+
+    MBuffer &MBuffer::put(int64_t val) noexcept
+    {
+        auto val_no = static_cast<int64_t>(Net::HtoNll(val));
+
+        return put(reinterpret_cast<uint8_t *>(&val_no), sizeof(int64_t));
+    }
+
+    MBuffer &MBuffer::put(float val) noexcept
+    {
+        auto val_no = Net::HtoNf(val);
+
+        return put(reinterpret_cast<uint8_t *>(&val_no), sizeof(float));
+    }
+
+    MBuffer &MBuffer::put(double val) noexcept
+    {
+        auto val_no = Net::HtoNd(val);
+
+        return put(reinterpret_cast<uint8_t *>(&val_no), sizeof(double));
+    }
+
+    MBuffer &MBuffer::put(bool val) noexcept
+    {
+        return put(reinterpret_cast<uint8_t *>(&val), sizeof(bool));
+    }
+
+    bool MBuffer::get(uint8_t &val, size_t index) const noexcept
+    {
+        if (!hasEnoughSpace(index, sizeof(uint8_t)))
+            return false;
+
+        val = m_Buffer[index];
+
+        return true;
+    }
+
+    bool MBuffer::get(int8_t &val, size_t index) const noexcept
+    {
+        if (!hasEnoughSpace(index, sizeof(int8_t)))
+            return false;
+
+        val = static_cast<int8_t>(m_Buffer[index]);
+
+        return true;
+    }
+
+    bool MBuffer::get(uint16_t &val, size_t index) const noexcept
+    {
+        if (!hasEnoughSpace(index, sizeof(uint16_t)))
+            return false;
+
+        uint16_t temp = *reinterpret_cast<uint16_t *>(&m_Buffer[index]);
+        val = ntohs(temp);
+
+        return true;
+    }
+
+    bool MBuffer::get(int16_t &val, size_t index) const noexcept
+    {
+        if (!hasEnoughSpace(index, sizeof(int16_t)))
+            return false;
+
+        int16_t temp = *reinterpret_cast<int16_t *>(&m_Buffer[index]);
+        val = static_cast<int16_t>(ntohs(temp));
+
+        return true;
+    }
+
+    bool MBuffer::get(uint32_t &val, size_t index) const noexcept
+    {
+        if (!hasEnoughSpace(index, sizeof(uint32_t)))
+            return false;
+
+        uint32_t temp = *reinterpret_cast<uint32_t *>(&m_Buffer[index]);
+        val = ntohl(temp);
+
+        return true;
+    }
+
+    bool MBuffer::get(int32_t &val, size_t index) const noexcept
+    {
+        if (!hasEnoughSpace(index, sizeof(int32_t)))
+            return false;
+
+        int32_t temp = *reinterpret_cast<int32_t *>(&m_Buffer[index]);
+        val = static_cast<int32_t>(ntohl(temp));
+
+        return true;
+    }
+
+    bool MBuffer::get(uint64_t &val, size_t index) const noexcept
+    {
+        if (!hasEnoughSpace(index, sizeof(uint64_t)))
+            return false;
+
+        uint64_t temp = *reinterpret_cast<uint64_t *>(&m_Buffer[index]);
+        val = Net::NtoHll(temp);
+
+        return true;
+    }
+
+    bool MBuffer::get(int64_t &val, size_t index) const noexcept
+    {
+        if (!hasEnoughSpace(index, sizeof(int64_t)))
+            return false;
+
+        int64_t temp = *reinterpret_cast<int64_t *>(&m_Buffer[index]);
+        val = static_cast<int64_t>(Net::NtoHll(temp));
+
+        return true;
+    }
+
+    bool MBuffer::get(float &val, size_t index) const noexcept
+    {
+        if (!hasEnoughSpace(index, sizeof(float)))
+            return false;
+
+        float temp = *reinterpret_cast<float *>(&m_Buffer[index]);
+        val = Net::NtoHf(temp);
+
+        return true;
+    }
+
+    bool MBuffer::get(double &val, size_t index) const noexcept
+    {
+        if (!hasEnoughSpace(index, sizeof(double)))
+            return false;
+
+        double temp = *reinterpret_cast<double *>(&m_Buffer[index]);
+        val = Net::NtoHd(temp);
+
+        return true;
+    }
+
+    bool MBuffer::get(bool &val, size_t index) const noexcept
+    {
+        if (!hasEnoughSpace(index, sizeof(bool)))
+            return false;
+
+        val = static_cast<bool>(m_Buffer[index]);
+
+        return true;
+    }
+
+    uint8_t MBuffer::operator[](size_t index) const noexcept
+    {
+        if (index < 0 || index >= m_Buffer.size())
+            return 0;
+
+        return m_Buffer[index];
+    }
+
+    MBuffer &MBuffer::operator<<(uint8_t val) noexcept
+    {
+        return put(val);
+    }
+
+    MBuffer &MBuffer::operator<<(int8_t val) noexcept
+    {
+        return put(val);
+    }
+
+    MBuffer &MBuffer::operator<<(uint16_t val) noexcept
+    {
+        return put(val);
+    }
+
+    MBuffer &MBuffer::operator<<(int16_t val) noexcept
+    {
+        return put(val);
+    }
+
+    MBuffer &MBuffer::operator<<(uint32_t val) noexcept
+    {
+        return put(val);
+    }
+
+    MBuffer &MBuffer::operator<<(int32_t val) noexcept
+    {
+        return put(val);
+    }
+
+    MBuffer &MBuffer::operator<<(uint64_t val) noexcept
+    {
+        return put(val);
+    }
+
+    MBuffer &MBuffer::operator<<(int64_t val) noexcept
+    {
+        return put(val);
+    }
+
+    MBuffer &MBuffer::operator<<(float val) noexcept
+    {
+        return put(val);
+    }
+
+    MBuffer &MBuffer::operator<<(double val) noexcept
+    {
+        return put(val);
+    }
+
+    MBuffer &MBuffer::operator<<(bool val) noexcept
+    {
+        return put(val);
+    }
+
+    void MBuffer::operator>>(uint8_t &val) const noexcept
+    {
+        get(val);
+    }
+
+    void MBuffer::operator>>(int8_t &val) const noexcept
+    {
+        get(val);
+    }
+
+    void MBuffer::operator>>(uint16_t &val) const noexcept
+    {
+        get(val);
+    }
+
+    void MBuffer::operator>>(int16_t &val) const noexcept
+    {
+        get(val);
+    }
+
+    void MBuffer::operator>>(uint32_t &val) const noexcept
+    {
+        get(val);
+    }
+
+    void MBuffer::operator>>(int32_t &val) const noexcept
+    {
+        get(val);
+    }
+
+    void MBuffer::operator>>(uint64_t &val) const noexcept
+    {
+        get(val);
+    }
+
+    void MBuffer::operator>>(int64_t &val) const noexcept
+    {
+        get(val);
+    }
+
+    void MBuffer::operator>>(float &val) const noexcept
+    {
+        get(val);
+    }
+
+    void MBuffer::operator>>(double &val) const noexcept
+    {
+        get(val);
+    }
+
+    void MBuffer::operator>>(bool &val) const noexcept
+    {
+        get(val);
+    }
+
+    bool MBuffer::hasEnoughSpace(size_t index, size_t size) const noexcept
+    {
+        if (index < 0 || index >= m_Buffer.size())
+            return false;
+
+        return m_Buffer.size() - index >= size;
+    }
+
+    MBuffer &MBuffer::put(uint8_t *data, size_t size) noexcept
+    {
+        for (size_t i{}; i < size; i++)
+            m_Buffer.push_back(data[i]);
+
+        return *this;
     }
 }
