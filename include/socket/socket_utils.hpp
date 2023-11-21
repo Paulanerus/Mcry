f0252ce08e181d1a83267eeffb50b6da9fc007c5
Paul <41129807+Paulanerus@users.noreply.github.com>
2023-11-21 18:08:13 +0100

    Update socket_utils.hpp to include ostream header and add operator<< for ConnectionInfo struct

--- include/socket/socket_utils.hpp
+++ include/socket/socket_utils.hpp
@@ -1,22 +1,23 @@
 #pragma once
 
 #include <cstdint>
-#include <string_view>
+#include <string>
+#include <ostream>
 
 #include "../buffer/mbuffer.hpp"
 
 namespace Mcry
 {
-    enum class Protocol
+    enum class Protocol : uint8_t
     {
-        TCP,
-        UDP,
-        RUDP,
+        TCP = 0,
+        UDP = 1,
+        RUDP = 2,
     };
 
     struct ConnectionInfo
     {
-        std::string_view address;
+        std::string address;
         uint16_t port;
         bool is_ipv6 = false;
         bool is_unix = false;
@@ -35,4 +36,10 @@ namespace Mcry
         uint16_t backlog;
         bool auto_accept;
     };
+}
+
+std::ostream &operator<<(std::ostream &os, const Mcry::ConnectionInfo &info)
+{
+    return os << "ConnectionInfo{address: " << info.address << ", port: " << info.port << ", IPv6: " << info.is_ipv6
+              << ", Unix: " << info.is_unix << ", Protocol: " << static_cast<uint16_t>(info.protocol) << "}";
 }
